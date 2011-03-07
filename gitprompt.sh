6cd8a5611808a01fda77a6ee7486a44ffe356a0c
Kris Steinhoff <steinhof@umich.edu>
2011-03-07 15:39:28 -0500

    fixed a few bugs

--- gitprompt.sh
+++ gitprompt.sh
@@ -32,55 +32,51 @@ COLOR_CYAN_BOLD="\e[36;0m"
 
 COLOR_NONE="\e[0m"
 
+if [ $UID -eq 0 ]; then
+    export PROMPT_CHAR="#"
+else
+    export PROMPT_CHAR="$"
+fi
+
 git-prompt()
 {
+    PREV_RET_VAL=$?;
+
     PS1=""
 
-        PS1="${PS1}${COLOR_PROMPT_YELLOW}\u${COLOR_PROMPT_NONE}"
-        PS1="${PS1}@${COLOR_PROMPT_RED}\h${COLOR_PROMPT_NONE}:${COLOR_PROMPT_YELLOW}\w${COLOR_PROMPT_NONE} ${COLOR_PROMPT_CYAN}\t${COLOR_PROMPT_NONE}"
+    PS1="${PS1}${COLOR_PROMPT_YELLOW}\u${COLOR_PROMPT_NONE}"
+    PS1="${PS1}@${COLOR_PROMPT_RED}\h${COLOR_PROMPT_NONE}:${COLOR_PROMPT_YELLOW}\w${COLOR_PROMPT_NONE} ${COLOR_PROMPT_CYAN}\t${COLOR_PROMPT_NONE}"
 
-        git branch &> /dev/null; rc=$?;
-        if [ $rc -eq 0 ]; then
-            BRANCH=`git branch --no-color 2> /dev/null | grep ^\* | sed s/\*\ //g`
-            if [ "x${BRANCH}" = "x" ]; then
-                INFO="(no branch found)"
-            else
-                INFO="${BRANCH}:"
-            fi
-            COMMIT=`git log -1 2> /dev/null | head -1 | awk '{print $2}' | cut -c 1-7`
-            INFO=${INFO}${COMMIT};
-            git status 2> /dev/null | tail -n1 | grep 'working directory clean' &> /dev/null; rc=$?
-            if [ $rc -eq 0 ]; then
-                STATE_COLOR=${COLOR_PROMPT_GREEN}
-            else
-                STATE_COLOR=${COLOR_PROMPT_RED}
-            fi
-            PS1="${PS1} ${STATE_COLOR}[${INFO}]${COLOR_PROMPT_NONE}"
-
-            for remote in `git remote`; do
-                remote_branch=`git remote show -n ${remote} | grep "${BRANCH} *merges with remote" | awk -F" merges with remote " '{print $2}'`
-                if [ "x$remote_branch" != "x" ]; then
-                    PS1="${PS1} ${COLOR_PROMPT_MAGENTA}${remote}/${remote_branch}${COLOR_PROMPT_NONE}"
-                fi
-            done
-        fi
-
-        svn info &> /dev/null; rc=$?
-        if [ $rc -eq 0 ]; then
-            REV=`svn info | grep '^Revision: '| awk -F': ' '{print $2}'`
-            lines=`svn st | wc | awk '{print $1}'`
-            if [ $lines -eq 0 ]; then
-                STATE_COLOR=${COLOR_PROMPT_GREEN}
-            else
-                STATE_COLOR=${COLOR_PROMPT_RED}
-            fi
-            PS1="${PS1} ${STATE_COLOR}[${REV}]${COLOR_PROMPT_NONE}"
-        fi
-
-        if test $PREV_RET_VAL -eq 0
-        then
-            PS1="${PS1}\n${COLOR_PROMPT_GREEN}${PROMPT_CHAR}${COLOR_PROMPT_NONE} "
+    git branch &> /dev/null; rc=$?;
+    if [ $rc -eq 0 ]; then
+        BRANCH=`git branch --no-color 2> /dev/null | grep ^\* | sed s/\*\ //g`
+        if [ "x${BRANCH}" = "x" ]; then
+            INFO="(no branch found)"
         else
-            PS1="${PS1}\n${COLOR_PROMPT_RED}[${PREV_RET_VAL}] ${PROMPT_CHAR}${COLOR_PROMPT_NONE} "
+            INFO="${BRANCH}:"
         fi
+        COMMIT=`git log -1 2> /dev/null | head -1 | awk '{print $2}' | cut -c 1-7`
+        INFO=${INFO}${COMMIT};
+        git status 2> /dev/null | tail -n1 | grep 'working directory clean' &> /dev/null; rc=$?
+        if [ $rc -eq 0 ]; then
+            STATE_COLOR=${COLOR_PROMPT_GREEN}
+        else
+            STATE_COLOR=${COLOR_PROMPT_RED}
+        fi
+        PS1="${PS1} ${STATE_COLOR}[${INFO}]${COLOR_PROMPT_NONE}"
+
+        for remote in `git remote`; do
+            remote_branch=`git remote show -n ${remote} | grep "${BRANCH} *merges with remote" | awk -F" merges with remote " '{print $2}'`
+            if [ "x$remote_branch" != "x" ]; then
+                PS1="${PS1} ${COLOR_PROMPT_MAGENTA}${remote}/${remote_branch}${COLOR_PROMPT_NONE}"
+            fi
+        done
+    fi
+
+    if test $PREV_RET_VAL -eq 0
+    then
+        PS1="${PS1}\n${COLOR_PROMPT_GREEN}${PROMPT_CHAR}${COLOR_PROMPT_NONE} "
+    else
+        PS1="${PS1}\n${COLOR_PROMPT_RED}[${PREV_RET_VAL}] ${PROMPT_CHAR}${COLOR_PROMPT_NONE} "
+    fi
 }
